29cee6eadb2b7a08dba702cc6b28f1ac16d8dfe8
julianocmr60 <julianorossi60@gmail.com>
2021-11-11 20:53:13 -0200

    commit index module

---
+++ src/app/index-module/index-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IndexModuleRoutingModule } from './index-module-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IndexModuleRoutingModule
+  ]
+})
+export class IndexModuleModule { }
